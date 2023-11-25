7faac922054cca10711d95498c51ca56971353d2
Enayet <enayetflweb@gamil.com>
2023-11-25 16:23:23 +0600

    use admin hook create and dashboard layout button conditional rendering

--- src/MainLayout/DashboardLayout.jsx
+++ src/MainLayout/DashboardLayout.jsx
@@ -2,21 +2,30 @@ import { Link, Outlet } from "react-router-dom";
 import { AwesomeButton } from "react-awesome-button";
 import 'react-awesome-button/dist/styles.css';
 import '../Components/button.css'
+import useAdmin from "../hooks/useAdmin";
+
 
 const DashboardLayout = () => {
+  const [isAdmin] = useAdmin()
   return (
     <div className="flex justify-between">
       {/* sidebar */}
     <div className="min-h-screen bg-five p-10 space-y-5 flex flex-col">
-      <Link to='/dashboard/myProfile'>
+      {
+        isAdmin ? <>
+        <Link to='/dashboard/myProfile'>
       <AwesomeButton type="primary" className='aws-btn font-bold px-20 w-full' >My Profile</AwesomeButton>
       </Link>
       <Link to='/dashboard/requestedMeals'>
       <AwesomeButton type="primary" className='aws-btn font-bold px-20 w-full' >Requested Meals</AwesomeButton>
       </Link>
-      <Link to='/dashboard/myReviews'>
+     
+        </> : <>
+        <Link to='/dashboard/myReviews'>
       <AwesomeButton type="primary" className='aws-btn font-bold px-20 w-full' >My Reviews</AwesomeButton>
       </Link>
+        </>
+      }
     </div>
       {/* main section */}
       <div>
@@ -25,5 +34,6 @@ const DashboardLayout = () => {
     </div>
   );
 };
+
 
 export default DashboardLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './Provider/AuthProvider.jsx'
 import router from './Router/router.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <QueryClientProvider client={queryClient}>
    <AuthProvider>
    <RouterProvider router={router}/>
    </AuthProvider>
+   </QueryClientProvider>
   </React.StrictMode>,
 )
 
