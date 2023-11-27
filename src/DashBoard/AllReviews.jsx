795a46c1c641f44c078e01fc04d0e89e67a968e2
Enayet <enayetflweb@gamil.com>
2023-11-27 22:08:43 +0600

    all review data fetch, view button activate

--- src/DashBoard/AllReviews.jsx
+++ src/DashBoard/AllReviews.jsx
@@ -1,5 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import { Link } from "react-router-dom";
 
 const AllReviews = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const {data, isLoading} = useQuery({
+    queryKey:['allReview'],
+    queryFn: async () => {
+      const result = await axiosSecure.get('/api/v1/allreview')
+      return result.data
+    }
+  })
+  if(isLoading){
+    return <p>Loading</p>
+  }
+
+  console.log(data)
   return (
     <div>
       <div className="overflow-x-auto">
@@ -15,17 +32,25 @@ const AllReviews = () => {
     </thead>
     <tbody className="bg-white divide-y divide-gray-200">
       {/* Row 1: Fake Data */}
-      <tr>
-        <td className="px-6 py-4 whitespace-nowrap">Spicy Tacos</td>
-        <td className="px-6 py-4 whitespace-nowrap">110</td>
-        <td className="px-6 py-4 whitespace-nowrap">90</td>
+      {
+        data.length > 0 ? (
+          data.map((review, index) => (
+            <tr key={index}>
+        <td className="px-6 py-4 whitespace-nowrap">{review.meal.mealTitle}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{review.meal.likes}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{review.meal.reviews}</td>
         <td className="px-6 py-4 whitespace-nowrap">
           <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
         </td>
         <td className="px-6 py-4 whitespace-nowrap">
+          <Link to={`/mealdetails/${review.meal._id}`}>
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">View</button>
+          </Link>
         </td>
       </tr>
+          ))
+        ) : (<p>No review to show</p>)
+      }
     </tbody>
   </table>
 </div>
