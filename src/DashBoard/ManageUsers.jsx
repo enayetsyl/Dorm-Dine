201cc5838e3085954d4810c62224d290ecf77ce8
Enayet <enayetflweb@gamil.com>
2023-11-27 20:30:59 +0600

    manage user functionality done

--- src/DashBoard/ManageUsers.jsx
+++ src/DashBoard/ManageUsers.jsx
@@ -1,5 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import swal from "sweetalert";
 
 const ManageUsers = () => {
+  const {user, googleUser} = useAuth()
+  
+  const axiosSecure = useAxiosSecure()
+
+  const {data, isLoading, refetch} = useQuery({
+    queryKey:['allUser'],
+    queryFn: async () => {
+      const response = await axiosSecure.get('/api/v1/allUser')
+      return response.data;
+    }
+  })
+
+  const handleStatusChange = async(id) => {
+    const updateStatusId = await axiosSecure.patch(`/api/v1/makeadmin/${id}`)
+    if(updateStatusId.data.modifiedCount > 0){
+      swal('Congratulations', 'Your make a new admin', 'success');
+      console.log(updateStatusId)
+      refetch()
+    }
+  }
+
+  if(isLoading){
+    return <p>Loading</p>
+  }
+  console.log(data)
+
+  console.log('user', user)
+  console.log('google user',googleUser)
   return (
     <div className="w-[90%] mx-auto">
       <div className="overflow-x-auto">
@@ -14,14 +46,32 @@ const ManageUsers = () => {
     </thead>
     <tbody className="bg-white divide-y divide-gray-200">
       {/* Row 1: Fake Data */}
-      <tr>
-        <td className="px-6 py-4 whitespace-nowrap">John Doe</td>
-        <td className="px-6 py-4 whitespace-nowrap">john.doe@example.com</td>
+     {
+      data.length > 0 ? (
+        
+          data.map((user, index) => (
+            <tr key={index}>
+        <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
         <td className="px-6 py-4 whitespace-nowrap">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Make Admin</button>
+          {
+            user.role !== 'admin' ? (
+              <button 
+          onClick={() => handleStatusChange(user._id)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Make Admin</button>
+            ) :(
+              <button 
+          disabled
+          className="bg-three text-white font-bold py-2 px-4 rounded">Make Admin</button>
+            )
+          }
         </td>
-        <td className="px-6 py-4 whitespace-nowrap">Active</td>
+        <td className="px-6 py-4 whitespace-nowrap capitalize">{user.package}</td>
       </tr>
+          ))
+        
+      ) : (<p>No user to show try again later.</p>)
+     }
     </tbody>
   </table>
 </div>
